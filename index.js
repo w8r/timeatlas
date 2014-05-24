5d621e7aa56b41a991f8d748067347ef3a0d23a0
w8r <info@w8r.name>
2014-05-24 22:51:03 +0400

    experiments

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ var logfmt = require("logfmt");
 var app = express();
 
 app.use(logfmt.requestLogger());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+    // res.send('Hello World!');
+    res.render('index', {
+        title: 'Home',
+        content: 'jade tttt'
+    })
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
